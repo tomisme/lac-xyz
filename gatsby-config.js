9841df24f76428cbb1cab340d3c71b79db168908
Tom Hutchinson <git@tomisme.com>
2020-05-03 17:10:00 +0800

    why was that even there?

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,10 +95,10 @@ module.exports = {
         // Set how many pages are retrieved per API request.
         perPage: 100,
         // Search and Replace Urls across WordPress content.
-        searchAndReplaceContentUrls: {
-          sourceUrl: "https://wp.lightsandclockwork.xyz",
-          replacementUrl: "https://localhost:8000",
-        },
+        // searchAndReplaceContentUrls: {
+        //   sourceUrl: "https://wp.lightsandclockwork.xyz",
+        //   replacementUrl: "https://localhost:8000",
+        // },
         // Set how many simultaneous requests are sent at once.
         concurrentRequests: 10,
         // Set WP REST API routes whitelists
