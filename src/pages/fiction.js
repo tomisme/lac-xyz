8dbe9d28ebb36ae9a380062ad567459b6ea117f0
Tom Hutchinson <git@tomisme.com>
2020-07-22 18:12:12 +0800

    Fiction

--- src/pages/fiction.js
+++ src/pages/fiction.js
@@ -7,51 +7,45 @@ import PostedBy from "../components/PostedBy"
 
 export const query = graphql`
   query {
-    allWordpressPost(filter: {categories: {elemMatch: {name: {eq: "When the Bell Goes"}}}}) {
+    allWordpressCategory(filter: {wordpress_parent: {eq: 45}, count: {gt: 0}}) {
       edges {
         node {
+          name
+          description
+          wordpress_parent
+          count
           path
-          title
-          excerpt
-          slug
-          author {
-            name
-          }
-          date(formatString: "MMMM DD, YYYY")
-          categories {
-            name,
-            slug,
-            path
-          }
         }
       }
     }
   }
 `
 
-const IndexPage = ({ data }) => (
+const FictionLayout = ({ data }) => (
   <Layout>
-    <SEO title="Home" keywords={[]} />
-    <h1>Recently Posted Fiction</h1>
-    <ul class="post-list" style={{ listStyle: "none" }}>
-      {data.allWordpressPost.edges.map(post => (
-        <li style={{ padding: "20px 0", borderBottom: "1px solid #ccc" }}>
-          <Link
-            to={post.node.path}
-            style={{ display: "flex", color: "black", textDecoration: "none" }}
-          >
-            <div style={{}}>
-              <h3
-                dangerouslySetInnerHTML={{ __html: post.node.title }}
-              />
-              <PostedBy {...post.node} />
-              <div dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
-            </div>
-          </Link>
-        </li>
+    <SEO title="Fiction" keywords={[]} />
+    <h1>Fiction</h1>
+    <ul class="post-list" style={{ listStyle: "none", margin: 0 }}>
+      {data.allWordpressCategory.edges.map(category => (
+        <Link
+          to={category.node.path}
+          style={{ display: "flex", color: "black", textDecoration: "none" }}
+        >
+          <li style={{
+            padding: "20px",
+            borderBottom: "1px solid #ccc",
+            background: "aliceblue"
+          }}>
+            <h3
+              dangerouslySetInnerHTML={{ __html: category.node.name }}
+            />
+            <h4 class="fiction-subheading">By Elisa</h4>
+            <div dangerouslySetInnerHTML={{ __html: category.node.description }} />
+          </li>
+        </Link>
       ))}
     </ul>
   </Layout>
 )
 
-export default IndexPage
+export default FictionLayout
